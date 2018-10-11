9bfa8c3b219a447555083cc52196c7364a991b4f
alien190 <alien1900@bk.ru>
2018-10-11 15:52:56 +0700

    first service

--- app/src/main/java/com/example/alien/evernoteandroidjob/DemoSyncJob.java
+++ app/src/main/java/com/example/alien/evernoteandroidjob/DemoSyncJob.java
@@ -1,20 +1,105 @@
 package com.example.alien.evernoteandroidjob;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import com.evernote.android.job.Job;
 import com.evernote.android.job.JobRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class DemoSyncJob extends Job {
 
     public static final String TAG = "job_demo_tag";
+    public static final int UPDATE_INTERVAL = 5000;
+    public static final int UPDATE_FASTEST_INTERVAL = 2000;
+    public static final int UPDATE_MIN_DISTANCE = 10;
+
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private LocationRequest mLocationRequest = new LocationRequest();
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Log.d(TAG, "onLocationResult: ");
+            if (locationResult != null) {
+                Location location = locationResult.getLastLocation();
+                LatLng newPosition = new LatLng(location.getLatitude(), location.getLongitude());
+                //mTrackHelper.onRouteUpdate(newPosition);
+                Log.d(TAG, "onLocationResult: " + newPosition.toString());
+                mDoneSignal.countDown();
+            }
+        }
+    };
+    private CountDownLatch mDoneSignal;
 
     @Override
     @NonNull
+
     protected Result onRunJob(Params params) {
         Log.d(TAG, "onRunJob: ");
-        // run your job here
+
+
+        mDoneSignal = new CountDownLatch(1);
+
+
+        new HandlerThread("proc") {
+            @Override
+            public void run() {
+                Looper.prepare();
+
+                mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+                mLocationRequest.setInterval(UPDATE_INTERVAL);
+                mLocationRequest.setFastestInterval(UPDATE_FASTEST_INTERVAL);
+                mLocationRequest.setSmallestDisplacement(UPDATE_MIN_DISTANCE);
+                mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+                try {
+                    if (ContextCompat.checkSelfPermission(getContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        Log.d(TAG, "onRunJob: PERMISSION_GRANTED ---------------------------------------------------------------------------------");
+                        //mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+                        mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                            @Override
+                            public void onLocationResult(LocationResult locationResult) {
+                                Log.d(TAG, "onLocationResult: ");
+                            }
+
+                            @Override
+                            public void onLocationAvailability(LocationAvailability locationAvailability) {
+                                Log.d(TAG, "onLocationAvailability: ");
+                            }
+                        }, Looper.myLooper());
+                    }
+                } catch (Throwable e) {
+                    Log.d(TAG, "onRunJob: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        try {
+            Log.d(TAG, "onRunJob: await---------------------------------------------------------------------------------");
+            mDoneSignal.await(30, TimeUnit.SECONDS);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
         return Result.RESCHEDULE;
     }
 
--- app/src/main/java/com/example/alien/evernoteandroidjob/MainActivity.java
+++ app/src/main/java/com/example/alien/evernoteandroidjob/MainActivity.java
@@ -1,22 +1,57 @@
 package com.example.alien.evernoteandroidjob;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TimeUtils;
 
 import com.evernote.android.job.JobManager;
 import com.evernote.android.job.JobRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.Task;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import static com.example.alien.evernoteandroidjob.DemoSyncJob.UPDATE_INTERVAL;
+
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "job_demo_tag";
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Log.d(TAG, "onLocationResult: ");
+            if (locationResult != null) {
+                Location location = locationResult.getLastLocation();
+                LatLng newPosition = new LatLng(location.getLatitude(), location.getLongitude());
+                //mTrackHelper.onRouteUpdate(newPosition);
+                Log.d(TAG, "onLocationResult: " + newPosition.toString());
+
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         JobManager.instance().cancelAll();
-        schedulePeriodicJob();
-       // scheduleOneTimeJob();
+        //schedulePeriodicJob();
+        //scheduleOneTimeJob();
+        getLocation();
     }
 
     private void schedulePeriodicJob() {
@@ -28,10 +63,57 @@ public class MainActivity extends AppCompatActivity {
 
     private void scheduleOneTimeJob() {
         int jobId = new JobRequest.Builder(DemoSyncJob.TAG)
-                //.setExact(TimeUnit.SECONDS.toMillis(1))
-                .setExecutionWindow(TimeUnit.SECONDS.toMillis(30), TimeUnit.SECONDS.toMillis(60))
+                .setExact(TimeUnit.SECONDS.toMillis(1))
+                //.setExecutionWindow(TimeUnit.SECONDS.toMillis(30), TimeUnit.SECONDS.toMillis(60))
                 .setBackoffCriteria(TimeUnit.SECONDS.toMillis(1), JobRequest.BackoffPolicy.LINEAR)
                 .build()
                 .schedule();
     }
+
+    private void getLocation() {
+        new HandlerThread("proc") {
+            @Override
+            public void run() {
+                Looper.prepare();
+                final CountDownLatch mDoneSignal = new CountDownLatch(1);
+
+                final int UPDATE_INTERVAL = 5000;
+                final int UPDATE_FASTEST_INTERVAL = 2000;
+                final int UPDATE_MIN_DISTANCE = 10;
+                final String TAG = "job_demo_tag";
+
+                FusedLocationProviderClient mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+                LocationRequest mLocationRequest = new LocationRequest();
+                mLocationRequest.setInterval(UPDATE_INTERVAL);
+                mLocationRequest.setFastestInterval(UPDATE_FASTEST_INTERVAL);
+                mLocationRequest.setSmallestDisplacement(UPDATE_MIN_DISTANCE);
+                mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                Log.d(TAG, "onRunJob: ");
+                try {
+                    if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        Log.d(TAG, "onRunJob: PERMISSION_GRANTED ---------------------------------------------------------------------------------");
+                        Task<Location> resTask = mFusedLocationProviderClient.getLastLocation();
+                        int sleepCount = 0;
+                        while (!resTask.isComplete() && sleepCount < 300) {
+                            Thread.sleep(100);
+                            Log.d(TAG, "getLocation: sleep");
+                            sleepCount++;
+                        }
+                        Log.d(TAG, "getLocation: " + resTask.getResult().toString());
+                    }
+                } catch (Throwable e) {
+                    Log.d(TAG, "onRunJob: " + e.getMessage());
+                    e.printStackTrace();
+                }
+                try {
+                    //  mDoneSignal.await(30, TimeUnit.SECONDS);
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        }.start();
+    }
 }
+
